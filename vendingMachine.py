ae5b53c743e328377703a2995162f03842029c1d
Nagisa Yamano <nagisa.yamano@gmail.com>
2025-12-19 14:33:01 +0900

    入金額と飲み物のフィールドを追加

--- vendingMachine.py
+++ vendingMachine.py
@@ -1,4 +1,11 @@
 #プログラム本文．自販機のシミュレーションを作成する．
+maxStock = 5
 
-class vendingMachine():
-    pass
+class VendingMachine():
+    def __init__(self):
+        self.payment = 0
+        self.drinks = {
+            "water": maxStock,
+            "tea": maxStock,
+            "coke": maxStock
+        }
