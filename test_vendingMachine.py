fee7f1f84774b6de8eecda5bac49eacb8b782594
Nagisa Yamano <nagisa.yamano@gmail.com>
2025-12-19 14:32:17 +0900

    テスト項目1を追加

--- test_vendingMachine.py
+++ test_vendingMachine.py
@@ -1,8 +1,21 @@
 #自販機のテストを順に行う
 import vendingMachine
 
-print("-------テスト開始-------")
+def test01(vm):
+    '''
+    初期状態では
+    ・自販機内のお金は0円である．
+    ・初期状態では飲み物の在庫は充填されている．(満タンの本数を5本とする．)
+    '''
+    assert vm.payment == 0
+    for stock in vm.drinks.values():
+        assert stock == vendingMachine.maxStock
 
-vm = vendingMachine.vendingMachine()
+if __name__ == "__main__":
+    print("-------テスト開始-------")
+    vm1 = vendingMachine.VendingMachine()
 
-print("--全てのテストをクリア--")
+    test01(vm1)
+    print("test1: OK")
+
+    print("--全てのテストをクリア--")
