c643cfdcc1a23ce2ecc685ad1724a99e8f607527
Nagisa Yamano <nagisa.yamano@gmail.com>
2025-12-23 15:42:25 +0900

    飲み物に価格のフィールドを追加，飲み物のボタンを実装

--- vendingMachine.py
+++ vendingMachine.py
@@ -5,9 +5,14 @@ class VendingMachine():
     def __init__(self):
         self.payment = 0
         self.drinks = {
-            "water": maxStock,
-            "tea": maxStock,
-            "coke": maxStock
+            "water": {"stock": maxStock, "price":100},
+            "tea": {"stock": maxStock, "price":130},
+            "coke": {"stock": maxStock, "price":150}
         }
-    def insert_money(money):
+    def insert_money(self, money):
         self.payment += money
+    def buy_drinks(self, drink):
+        if self.payment >= self.drinks[drink]["price"]:
+            self.payment -= self.drinks[drink]["price"]
+            self.drinks[drink]["stock"] -= 1
+            return drink
