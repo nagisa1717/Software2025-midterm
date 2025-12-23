888dd32822317af0fa2eff58326b8efd9063eaaf
Nagisa Yamano <nagisa.yamano@gmail.com>
2025-12-23 15:48:38 +0900

    テスト項目4を追加

--- test_vendingMachine.py
+++ test_vendingMachine.py
@@ -6,7 +6,7 @@ def test01():
     vm = vendingMachine.VendingMachine()
     assert vm.payment == 0
     for drink in vm.drinks.values():
-        assert drink["stock"] == vendingMachine.maxStock #構造体の変更によりテストも変更
+        assert drink["stock"] == vendingMachine.maxStock
 
 def test02():
     vm = vendingMachine.VendingMachine()
@@ -20,12 +20,6 @@ def test02():
         vm.insert_money(money)
         assert vm.payment == before_payment + money
 
-'''
-ユーザーが飲み物のボタンを押すと，「飲み物の金額 <= 自販機内のお金」の場合，
-・ユーザーは押した飲み物が入手できる．
-・押した飲み物の在庫のみ1減る．
-・自販機内のお金が減る．
-'''
 def test03():
     # water
     vm1 = vendingMachine.VendingMachine()
@@ -51,14 +45,47 @@ def test03():
     assert vm3.payment == 200 - vm3.drinks["coke"]["price"]
     assert vm3.drinks["coke"]["stock"] == vendingMachine.maxStock - 1
 
+'''
+ユーザーが飲み物のボタンを押すと，「飲み物の金額 > 自販機内のお金」の場合，
+・ユーザーは何も入手しない．
+・飲み物の在庫は変化しない．
+・自販機内のお金は変化しない．
+'''
+def test04():
+    # water
+    vm1 = vendingMachine.VendingMachine()
+    vm1.insert_money(50)
+    result = vm1.buy_drinks("water")
+    assert result == None
+    assert vm1.payment == 50
+    assert vm1.drinks["water"]["stock"] == vendingMachine.maxStock
+
+    # tea
+    vm2 = vendingMachine.VendingMachine()
+    vm2.insert_money(0)
+    result = vm2.buy_drinks("tea")
+    assert result == None
+    assert vm2.payment == 0
+    assert vm2.drinks["tea"]["stock"] == vendingMachine.maxStock
+
+    # coke
+    vm3 = vendingMachine.VendingMachine()
+    vm3.insert_money(100)
+    result = vm3.buy_drinks("coke")
+    assert result == None
+    assert vm3.payment == 100
+    assert vm3.drinks["coke"]["stock"] == vendingMachine.maxStock
+
 if __name__ == "__main__":
     print("-------テスト開始-------")
-    # インスタンス生成はテストの中で行うように変更
+    
     test01()
     print("test1: OK")
     test02()
     print("test2: OK")
     test03()
     print("test3: OK")
+    test04()
+    print("test4: OK")
 
     print("--全てのテストをクリア--")
